23c04c753e2e738f0793e8f70115d7fce68e3a0c
hyegeun <63241747+hyegeun@users.noreply.github.com>
2021-02-27 00:16:04 +0900

    Create widget.js

---
+++ assets/js/plungins/widget.js
@@ -0,0 +1,12 @@
+(function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "right",          
+            widget: "n2s83zqxmxtskejv-bpdvsbctspgfiiww-yjrnqxbc74tzomku"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"))
